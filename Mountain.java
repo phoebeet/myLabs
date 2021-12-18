f2714e118ace660fee3069074440d421eaaf2035
Phoebe Yang <ph.yanzhe.yang@gmail.com>
2021-12-17 22:11:36 -0800

    Mountain Range lab
    v2
    
    Signed-off-by: Phoebe Yang <ph.yanzhe.yang@gmail.com>

--- Mountain.java
+++ Mountain.java
@@ -12,25 +12,31 @@ class Mountain {
     return m;
   }
 
-  public static String fillwater(String a) {
+  public static String fillwater(char[] c) {
     int p1, p2;
-    char[] c = a.toCharArray();
 
     p1 = p2 = -1;
     for (int i = 0; i < c.length; i++) {
-      if (c[i] == 'X') {
-        if (p1 == -1) {
-          p1 = i;
-          continue;
-        } else if (p2 == -1) {
-          p2 = i;
+      if ((i % 2) == 1)
+        continue;
+      else {
+        if (c[i] == 'X') {
+          if (p1 == -1) {
+            p1 = i;
+            continue;
+          } else if (p2 == -1) {
+            p2 = i;
+          }
         }
-      }
-      if (p1 != -1 && p2 != -1) {
-        for (int j = p1 + 1; j < p2; j++)
-          c[j] = 'o';
-        p1 = p2;
-        p2 = -1;
+        if (p1 != -1 && p2 != -1) {
+          for (int j = p1 + 1; j < p2; j++) {
+            if ((j % 2) == 1)
+              continue;
+            c[j] = 'o';
+          }
+          p1 = p2;
+          p2 = -1;
+        }
       }
     }
     return (new String(c));
@@ -39,19 +45,20 @@ class Mountain {
   public static void main(String[] args) {
     int[] h = new int[] {1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9};
     int mh = max(h);
-    String[] g = new String[mh];
+    char[] g = new char[2 * h.length - 1];
 
-    for(int i = 1; i <= mh; i++) {
-      g[mh - i] = "";
-      for (int j = 0; j < h.length; j++) {
-        if (i <= h[j])
-          g[mh - i] += "X";
-	else
-          g[mh - i] += " ";
+    for (int i = 0; i < mh; i++) {
+      for (int j = 0; j < g.length; j++) {
+        if ((j % 2) == 1)
+          g[j] = ' ';
+        else {
+          if (mh - i <= h[j/2])
+            g[j] = 'X';
+          else
+            g[j] = ' ';
+        }
       }
+      System.out.println(fillwater(g));
     }
-
-    for (int i = 0; i < mh; i++)
-      System.out.println(fillwater(g[i]));
   }
 }
